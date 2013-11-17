c0a76767e3eb5c3cacdc4d3d971acae65c9f9221
Walter Scheper <ratlaw@gmail.com>
2013-11-17 11:52:03 -0500

    Add virtualfish to activated plugins

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ set fish_theme ratlaw
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins aws pyenv
+set fish_plugins pyenv virtualfish
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -19,3 +19,13 @@ eval (dircolors -c "$HOME/.dircolors")
 
 # PYTHONSTARTUP
 set -x PYTHONSTARTUP "$HOME/.pythonrc.py"
+
+# add virtualfish
+set -x PIP_USE_WHEEL "true"
+set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
+set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
+
+. $fish_custom/plugins/virtualfish/virtual.fish
+. $fish_custom/plugins/virtualfish/auto_activation.fish
+. $fish_custom/plugins/virtualfish/global_requirements.fish
