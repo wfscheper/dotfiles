0fed108af9e34f1b56712827e96db4d1d38a88ec
Walter Scheper <ratlaw@gmail.com>
2014-11-10 19:30:00 +0000

    Add https://github.com/rupa/z

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,10 +14,15 @@ powerline-setup
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins pyenv virtualfish jump
+set fish_plugins z
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
+
+# z location
+if test -e "$HOME/.zbin"
+    set -x Z_SCRIPT_PATH "$HOME/.zbin/z.sh"
+end
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
