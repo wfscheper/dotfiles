2562c67aa0e545e847943327896ad673f940e9b6
Walter Scheper <ratlaw@gmail.com>
2015-08-28 22:25:14 -0400

    Remove oh-my-fish submodule
    oh-my-fish moved and is totally reworked.

--- config.fish
+++ config.fish
@@ -1,6 +1,3 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
 # add home bin dirs to path
 set -x PATH $HOME/bin $HOME/.local/bin $PATH
 
@@ -18,24 +15,14 @@ set -x PYTHONSTARTUP $HOME/.pythonrc.py
 
 # Theme
 powerline-daemon -q
-set -x POWERLINE_ROOT "$HOME/.local/lib/python2.6/site-packages/powerline"
-set fish_function_path $fish_function_path "$POWERLINE_ROOT/bindings/fish"
+set -x LOCAL_SITE_PKGS "$HOME/.local/lib/python2.7/site-packages"
+set fish_function_path $fish_function_path "$LOCAL_SITE_PKGS/powerline/bindings/fish"
 powerline-setup
 
 # dircolors
 if test -f $HOME/.dir_colors
     eval (dircolors -c $HOME/.dir_colors | sed 's/>&\/dev\/null$//')
 end
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
 
 . $fish_path/custom/plugins/virtualfish/virtual.fish
 set -x VIRTUAL_ENV_DISABLE_PROMPT true
