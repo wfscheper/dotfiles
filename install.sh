7bb55dd0729e0c694a956cfd5412ff5b093ba0c3
Walter Scheper <ratlaw@gmail.com>
2015-10-28 20:13:04 -0400

    Link omf dir in install script

--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@ set -e
 
 function link_file {
     source="${PWD}/$1"
-    if [[ -n "${2}" ]]; then
-        target="${HOME}/${2}"
+    if [[ -n "$2" ]]; then
+        target="${HOME}/$2"
     else
         target="${HOME}/${1/_/.}"
     fi
 
     if [ -e "${target}" ] && [ ! -L "${target}" ]; then
-        mv $target $target.df.bak
+        mv "${target}" "${target.df.bak}"
     fi
 
-    ln -sfn ${source} ${target}
+    ln -sfn "${source}" "${target}"
 }
 
 function unlink_file {
     source="${PWD}/$1"
-    if [[ -n "${2}" ]]; then
-        target="${HOME}/${2}"
+    if [[ -n "$2" ]]; then
+        target="${HOME}/$2"
     else
         target="${HOME}/${1/_/.}"
     fi
 
     if [ -e "${target}.df.bak" ] && [ -L "${target}" ]; then
-        unlink ${target}
-        mv $target.df.bak $target
+        unlink "${target}"
+        mv "${target.df.bak}" "${target}"
     fi
 }
 
@@ -34,26 +34,27 @@ case "$1" in
     vim)
         for i in _vim*
         do
-           link_file $i
+           link_file "$i"
         done
         ;;
     restore)
         for i in _*
         do
-            unlink_file $i
+            unlink_file "$i"
         done
         ;;
     *)
         for i in _*
         do
-            link_file $i
+            link_file "$i"
         done
 
         # install .config dirs
         for i in config/*; do
-            link_file $i .$i
+            link_file "$i" ".$i"
         done
-        link_file "ssh/config" ".ssh/config"
+        link_file local/share/omf .local/share/omf
+        link_file ssh/config .ssh/config
         chmod 0600 ~/.ssh/config
         ;;
 esac
