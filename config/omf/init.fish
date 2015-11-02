611ad2f0de2c6f52ecfea0741ee61fb03f4f57b3
Walter Scheper <Walter.Scheper@sas.com>
2015-11-02 16:24:27 -0500

    fish: push env variables into universal

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,27 +1,10 @@
 # configure syntax colors
-source $HOME/.config/fish/solarized.fish
-
-# add home bin dirs to path
-set -x PATH $HOME/bin $HOME/.local/bin /usr/local/opt/coreutils/libexec/gnubin \
-    $HOME/.gem/ruby/2.0.0/bin $PATH
-
-# setup homebrew manpath
-set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman /usr/local/share/man \
-    /usr/share/man
-
-# set editor
-set -x EDITOR '/usr/local/bin/vim'
-set -x GIT_EDITOR "$EDITOR"
+source $HOME/.config/omf/solarized.fish
 
 # set TERM
 if test "$TERM" = "xterm" -o "$TERM" = "screen"
     set -x TERM "$TERM-256color"
 end
-
-# set pythonrc
-set -x PYTHONSTARTUP $HOME/.pythonrc.py
-
-set -x LOCAL_SITE_PKGS "/usr/local/lib/python2.7/site-packages"
 
 # Theme
 powerline-daemon -q
@@ -31,16 +14,6 @@ powerline-setup
 # dircolors
 if test -f $HOME/.dir_colors
     eval (dircolors -c $HOME/.dir_colors | sed 's/>&\/dev\/null$//')
-end
-
-# Load /etc/profile.d
-for conf in /etc/profile.d/*.fish
-    source $conf
-end
-
-if which go > /dev/null ^&1
-    set -x GOPATH $HOME/go
-    set -x PATH $PATH $GOPATH/bin
 end
 
 if which git > /dev/null ^&1
@@ -53,7 +26,5 @@ end
 
 # load pyenv
 if test -d $HOME/.pyenv
-    set PYENV_ROOT $HOME/.pyenv
-    set -x PATH $PYENV_ROOT/bin $PATH
     status --is-interactive; and . (pyenv init -|psub)
 end
