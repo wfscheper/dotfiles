8c7f1706e285c9d0aa21894a66901e8b63da25ec
Walter Scheper <Walter.Scheper@sas.com>
2015-11-11 11:19:04 -0500

    Export variable globally in tmux updatenv
    This fixes an issue with the commands not working in the script, but working
    at the command-line.

--- config/omf/tmux.fish
+++ config/omf/tmux.fish
@@ -1,19 +1,19 @@
 function tmux -a cmd -d "Wraps tmux to provide updatenv command"
-    if test -z $cmd
-        command tmux
-    else
-        switch $cmd
-            case "updatenv"
-                for var in (command tmux showenv)
-                    switch $var
-                        case '-*'
-                            set -e (echo $var | sed 's/-//')
-                        case '*'
-                            eval set -x (echo $var | sed 's/=/ "/' | sed 's/$/"/')
-                    end
-                end
-            case "*"
-                command tmux $argv
+  if test -z $cmd
+    command tmux
+  else
+    switch $cmd
+      case "updatenv"
+        for var in (command tmux showenv)
+          switch $var
+            case '-*'
+              eval (echo $var | sed -e 's/-/set -eg /')
+            case '*'
+              eval (echo $var | sed -e 's/^/set -xg /' -e 's/=/ "/' -e 's/$/"/')
+          end
         end
+      case "*"
+        command tmux $argv
     end
+  end
 end
