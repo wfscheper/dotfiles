0474689bc5ecaf10e52caff583a5613bdb0113ce
Walter Scheper <Walter.Scheper@sas.com>
2015-12-09 15:56:03 -0500

    Add projects virtualfish plugin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -43,7 +43,7 @@ set -gx OMF_CONFIG $HOME/.config/omf
 
 # load virtualfish
 if test -f $LOCAL_SITE_PKGS/virtualfish/virtual.fish
-    eval (python -m virtualfish auto_activation global_requirements)
+    eval (python -m virtualfish auto_activation global_requirements projects)
 end
 
 # Load oh-my-fish configuration.
