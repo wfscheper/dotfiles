141c547732f21435d8622f784a776a9a369d00f0
Walter Scheper <ratlaw@gmail.com>
2016-11-04 09:48:08 -0400

    fish: move initialization into omf configuration
    Simply config.fish and move most of its contents into omf/before.init.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,64 +1,10 @@
-# set universal variables if they aren't set
-if not set -qU fish_user_paths
-    set -U fish_user_paths $HOME/.local/bin $HOME/bin
-    echo "fish_user_paths set to $fish_user_paths"
-end
-
-if not set -qU EDITOR
-    set -xU EDITOR (which vim)
-    echo "EDITOR set to $EDITOR"
-    set -xU GIT_EDITOR $EDITOR
-    echo "GIT_EDITOR set to $GIT_EDITOR"
-end
-
-if not set -qU LOCAL_SITE_PKGS
-    if test -d  ~/.local/lib/python2.7/site-packages
-        set -xU LOCAL_SITE_PKGS ~/.local/lib/python2.7/site-packages
-        echo "LOCAL_SITE_PKGS set to $LOCAL_SITE_PKGS"
-    end
-end
-
-if not set -qU PYTHONSTARTUP
-    set -xU PYTHONSTARTUP $HOME/.pythonrc.py
-    echo "PYTHONSTARTUP set to $PYTHONSTARTUP"
-end
-
-if not set -qU Z_SCRIPT_PATH
-    if test -d ~/.zbin
-        set -U Z_SCRIPT_PATH ~/.zbin/z.sh
-        echo "Z_SCRIPT_PATH set to $Z_SCRIPT_PATH"
-    end
-end
-
-if not set -qU GOPATH
-    if test -d $HOME/go
-        set -xU GOPATH $HOME/go
-        echo "GOPATH set to $GOPATH"
-        set -U fish_user_paths $fish_user_paths $GOPATH/bin
-        echo "fish_user_paths set to $fish_user_paths"
-    end
-end
-
-if not set -qU PYENV_ROOT
-    if test -d $HOME/.pyenv
-        set -xU PYENV_ROOT $HOME/.pyenv
-        echo "PYENV_ROOT set to $PYENV_ROOT"
-        set -U fish_user_paths $PYENV_ROOT/shims $PYENV_ROOT/bin $fish_user_paths
-        echo "fish_user_paths set to $fish_user_paths"
-    end
-end
-
 # Path to Oh My Fish install.
-set -gx OMF_PATH $HOME/.local/share/omf
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG $HOME/.config/omf
-
-# load virtualfish
-if test -f $LOCAL_SITE_PKGS/virtualfish/virtual.fish
-    set -x VIRTUALFISH_COMPAT_ALIASES true
-    eval (python -m virtualfish compat_aliases auto_activation global_requirements projects)
-end
+#set -gx OMF_CONFIG "{{OMF_CONFIG}}"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
---
+++ config/omf/before.init.fish
@@ -0,0 +1,55 @@
+# set universal variables if they aren't set
+if not set -qU fish_user_paths
+    set -U fish_user_paths $HOME/.local/bin $HOME/bin
+    echo "Added $HOME/.local/bin $HOME/bin to fish_user_paths"
+end
+
+if not set -qU EDITOR
+    set -xU EDITOR (which vim)
+    echo "EDITOR set to $EDITOR"
+    set -xU GIT_EDITOR $EDITOR
+    echo "GIT_EDITOR set to $GIT_EDITOR"
+end
+
+if not set -qU LOCAL_SITE_PKGS
+    if test -d  $HOME/.local/lib/python2.7/site-packages
+        set -xU LOCAL_SITE_PKGS $HOME/.local/lib/python2.7/site-packages
+        echo "LOCAL_SITE_PKGS set to $LOCAL_SITE_PKGS"
+    end
+end
+
+if not set -qU PYTHONSTARTUP
+    set -xU PYTHONSTARTUP $HOME/.pythonrc.py
+    echo "PYTHONSTARTUP set to $PYTHONSTARTUP"
+end
+
+if not set -qU Z_SCRIPT_PATH
+    if test -d $HOME/.zbin
+        set -U Z_SCRIPT_PATH $HOME/.zbin/z.sh
+        echo "Z_SCRIPT_PATH set to $Z_SCRIPT_PATH"
+    end
+end
+
+if not set -qU GOPATH
+    if test -d $HOME/go
+        set -xU GOPATH $HOME/go
+        echo "GOPATH set to $GOPATH"
+        set -U fish_user_paths $fish_user_paths $GOPATH/bin
+        echo "fish_user_paths set to $fish_user_paths"
+    end
+end
+
+if not set -qU PYENV_ROOT
+    if test -d $HOME/.pyenv
+        set -xU PYENV_ROOT $HOME/.pyenv
+        echo "PYENV_ROOT set to $PYENV_ROOT"
+        set -U fish_user_paths $PYENV_ROOT/shims $PYENV_ROOT/bin $fish_user_paths
+        echo "Added $PYENV_ROOT/shims $PYENV_ROOT/bin to fish_user_paths"
+    end
+end
+
+# load virtualfish
+if test -f $LOCAL_SITE_PKGS/virtualfish/virtual.fish
+    set -x VIRTUALFISH_COMPAT_ALIASES true
+    eval (python -m virtualfish compat_aliases auto_activation global_requirements projects)
+end
