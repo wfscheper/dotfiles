972c06790c75b492006711ba0544a4272202e00e
Walter Scheper <Walter.Scheper@sas.com>
2017-04-11 11:08:39 -0400

    Clean up fish config
    - Don't set universal varialbes in before.init. These only need to be
      set once, and the current implementation causes weird bugs with pyenv.
    - Remove git aliases that I don't use.

--- _config/omf/init.fish
+++ _config/omf/init.fish
@@ -15,15 +15,6 @@ if test -f $HOME/.dir_colors
     eval (dircolors -c $HOME/.dir_colors | sed 's/>&\/dev\/null$//')
 end
 
-if command -s git > /dev/null ^&1
-    alias gpull 'git pull'
-    alias gpush 'git push'
-    alias gci 'git commit'
-    alias gco 'git checkout'
-    alias gst 'git status'
-end
-
-# load pyenv
 if status --is-interactive
     source (pyenv init - | psub)
 
