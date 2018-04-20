8605817c68e6d004b100f594b2511415b37cb8db
Walter Scheper <Walter.Scheper@sas.com>
2018-04-20 14:38:04 -0400

    Switch to using py36 for personal site-packages.

--- _config/omf/before.init.fish
+++ _config/omf/before.init.fish
@@ -1,5 +1,5 @@
 # load virtualfish
 if test -f $LOCAL_SITE_PKGS/virtualfish/virtual.fish
     set -x VIRTUALFISH_COMPAT_ALIASES true
-    eval (python -m virtualfish compat_aliases auto_activation global_requirements projects)
+    eval (command python3 -m virtualfish compat_aliases auto_activation global_requirements projects)
 end
