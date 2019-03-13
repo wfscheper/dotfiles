0a623758761fa4dd6412da91625d604a4a8d4ed2
Walter Scheper <Walter.Scheper@sas.com>
2019-03-13 16:46:16 -0400

    Tweak pythonrc.py to work with modern python versions

--- _pythonrc.py
+++ _pythonrc.py
@@ -5,26 +5,29 @@ regular Python commands, so do what you will. Your ~/.inputrc file can greatly
 complement this file.
 
 """
+from tempfile import mkstemp
+from code import InteractiveConsole
 import os
 
 try:
     import readline
-    import rlcompleter
+    import rlcompleter  # noqa
     import atexit
 except ImportError:
     print("You need readline, rlcompleter, and atexit")
 
-
 # Make this work properly in Darwin and Linux
-if 'libedit' in readline.__doc__:                                          
-    readline.parse_and_bind("bind ^I rl_complete")                         
-else:                                                                      
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
     readline.parse_and_bind("tab: complete")
 
+
 class Completer(object):
+
     def __init__(self):
         # Enable a History
-        self.HISTFILE=os.path.expanduser("%s/.pyhistory" % os.environ["HOME"])
+        self.HISTFILE = os.path.expanduser("%s/.pyhistory" % os.environ["HOME"])
 
         # Read the existing history if there is one
         if os.path.exists(self.HISTFILE):
@@ -41,48 +44,51 @@ class Completer(object):
 
 c = Completer()
 
-WELCOME=''
+WELCOME = ''
+
+
 # Color Support
 class TermColors(dict):
     """Gives easy access to ANSI color codes. Attempts to fall back to no color
     for certain TERM values. (Mostly stolen from IPython.)"""
 
     COLOR_TEMPLATES = (
-        ("Black"       , "0;30"),
-        ("Red"         , "0;31"),
-        ("Green"       , "0;32"),
-        ("Brown"       , "0;33"),
-        ("Blue"        , "0;34"),
-        ("Purple"      , "0;35"),
-        ("Cyan"        , "0;36"),
-        ("LightGray"   , "0;37"),
-        ("DarkGray"    , "1;30"), ("LightRed"    , "1;31"),
-        ("LightGreen"  , "1;32"),
-        ("Yellow"      , "1;33"),
-        ("LightBlue"   , "1;34"),
-        ("LightPurple" , "1;35"),
-        ("LightCyan"   , "1;36"),
-        ("White"       , "1;37"),
-        ("Normal"      , "0"),
+        ("Black", "0;30"),
+        ("Red", "0;31"),
+        ("Green", "0;32"),
+        ("Brown", "0;33"),
+        ("Blue", "0;34"),
+        ("Purple", "0;35"),
+        ("Cyan", "0;36"),
+        ("LightGray", "0;37"),
+        ("DarkGray", "1;30"),
+        ("LightRed", "1;31"),
+        ("LightGreen", "1;32"),
+        ("Yellow", "1;33"),
+        ("LightBlue", "1;34"),
+        ("LightPurple", "1;35"),
+        ("LightCyan", "1;36"),
+        ("White", "1;37"),
+        ("Normal", "0"),
     )
 
     NoColor = ''
-    _base  = '\001\033[%sm\002'
+    _base = '\001\033[%sm\002'
 
     def __init__(self):
-        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux',
-                                    'screen', 'screen-256color', 'screen-bce'):
-            self.update(dict([(k, self._base % v) for k,v in self.COLOR_TEMPLATES]))
+        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux', 'screen', 'screen-256color', 'screen-bce'):
+            self.update(dict([(k, self._base % v) for k, v in self.COLOR_TEMPLATES]))
         else:
-            self.update(dict([(k, self.NoColor) for k,v in self.COLOR_TEMPLATES]))
+            self.update(dict([(k, self.NoColor) for k, v in self.COLOR_TEMPLATES]))
+
+
 _c = TermColors()
 
-
-
-import sys
+import sys  # noqa
 # Enable Color Prompts
 sys.ps1 = '%s>>> %s' % (_c['Green'], _c['Normal'])
 sys.ps2 = '%s... %s' % (_c['Red'], _c['Normal'])
+
 
 # Enable Pretty Printing for stdout
 def my_displayhook(value):
@@ -97,16 +103,17 @@ def my_displayhook(value):
         pprint.pprint(value)
         del pprint
 
+
 sys.displayhook = my_displayhook
+
 
 # Django Helpers
 def SECRET_KEY():
-    "Generates a new SECRET_KEY that can be used in a project settings file." 
+    "Generates a new SECRET_KEY that can be used in a project settings file."
 
     from random import choice
-    return ''.join(
-            [choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
-                for i in range(50)])
+    return ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+
 
 # If we're working with a Django project, set up the environment
 if 'DJANGO_SETTINGS_MODULE' in os.environ:
@@ -117,6 +124,7 @@ if 'DJANGO_SETTINGS_MODULE' in os.environ:
 
     class DjangoModels(object):
         """Loop through all the models in INSTALLED_APPS and import them."""
+
         def __init__(self):
             for m in get_models():
                 setattr(self, m.__name__, m)
@@ -147,16 +155,15 @@ Warning: DEBUG_PROPAGATE_EXCEPTIONS has been set to True.
 EDITOR = os.environ.get('EDITOR', 'vim')
 EDIT_CMD = '\e'
 
-from tempfile import mkstemp
-from code import InteractiveConsole
 
 class EditableBufferInteractiveConsole(InteractiveConsole):
+
     def __init__(self, *args, **kwargs):
-        self.last_buffer = [] # This holds the last executed statement
+        self.last_buffer = []  # This holds the last executed statement
         InteractiveConsole.__init__(self, *args, **kwargs)
 
     def runsource(self, source, *args):
-        self.last_buffer = [ source.encode('latin-1') ]
+        self.last_buffer = [source.encode('latin-1')]
         return InteractiveConsole.runsource(self, source, *args)
 
     def raw_input(self, *args):
@@ -169,10 +176,12 @@ class EditableBufferInteractiveConsole(InteractiveConsole):
             line = open(tmpfl).read()
             os.unlink(tmpfl)
             tmpfl = ''
-            lines = line.split( '\n' )
-            for i in range(len(lines) - 1): self.push( lines[i] )
+            lines = line.split('\n')
+            for i in range(len(lines) - 1):
+                self.push(lines[i])
             line = lines[-1]
         return line
+
 
 # clean up namespace
 del sys
