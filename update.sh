1ce68a3ab5c1d5f617f44af1f7aef0a7fba7b40d
Walter Scheper <Walter.Scheper@sas.com>
2020-10-05 10:01:28 -0400

    fix: add nvim to stowed packages

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-packages="bash fish git powerline python ssh tmux vim"
+packages="bash fish git nvim powerline python ssh tmux vim"
 if [[ "$(uname)" == Darwin ]]; then
     packages="${packages} iterm"
 fi
