b137028dc6a4af8efcb7e5232a8d35b6e22eccc9
Walter Scheper <walter.scheper@gmail.com>
2021-03-09 03:47:33 +0000

    fix(install): use portable redirect

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     pushd macos || exit
     brew bundle
     popd || exit
-elif command -v dnf &>/dev/null; then
+elif command -v dnf >/dev/null 2>&1; then
     xargs -a redhat/packages.txt sudo dnf install -y
-elif command -v apt-get &>dev/null; then
+elif command -v apt-get >/dev/null 2>&1; then
     sudo apt-get update
     xargs -a ubuntu/packages.txt sudo apt-get install -y
 fi
